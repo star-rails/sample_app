bb378f2edcc5c165882db6144388e6807dd5cd1b
star@neonan.com <your.email@example.com>
2013-05-16 19:17:43 +0800

    Finish user edit, update, index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  
+  def index
+    @user=User.all
+  end
 
   def new
-  	@user=	User.new
+  	@user=User.new
   end
 
   def show
-    @user=User.all
+    @user=User.find(params[:id])
   end
 
   def create
@@ -13,17 +19,40 @@ class UsersController < ApplicationController
   @user=User.new(params[:user])
 # 如果 user 对象能成功地保存进数据库
     if @user.save 
-      sign_in @user
-      flash[:success] =  '您已经注册成功'
-      redirect_to @user
-  	    
-# 发送邮件
-   #ActivateMail.deliver_sent(@user)
-   # redirect_to  :action  => 'success'
-# 保存失败
+       sign_in user
+       flash[:success] =  '您已经注册成功'
+       redirect_to user
     else
-  	  # render  "new"
-      redirect_to  new_user_path(@user)
+  	  render  "new"
+       # redirect_to  new_user_path(@user)
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+private
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_path, notice:"Please sign in." 
+    end
+end
+    def correct_user
+      @user =User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
